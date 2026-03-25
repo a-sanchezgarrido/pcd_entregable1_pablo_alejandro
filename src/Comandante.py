d77535bbb7ac197f601ef9c62c44aea3e21ffc4a
p-martinezlopez2 <p.martinezlopez2@um.es>
2026-03-25 10:58:26 +0100

    Try/except

--- src/Comandante.py
+++ src/Comandante.py
@@ -1,7 +1,7 @@
 from Usuario import Usuario
 from Repuesto import Repuesto
 from Nave import Nave
-from Errores import NaveNoComandada, RepuestoSinStock
+from Errores import NaveNoComandada, RepuestoSinStock, RepuestoDeOtraNave, RepuestoNoEncontrado
 
 class Comandante(Usuario):
     def __init__(self, id):
@@ -15,40 +15,62 @@ class Comandante(Usuario):
         nave.cambiar_comandante(self)
 
     def consultar_repuesto(self, nombre_repuesto, nombre_nave):
-        nave = None
-        for i in self.get_naves_asignadas():
-            if i.get_nombre() == nombre_nave:
-                nave = i
-                break
-        if nave is None:
-            raise NaveNoComandada(nombre_nave)
-        
-        if nave.comprobar_repuesto(nombre_repuesto):
+        try:
+            nave = None
+            for i in self.get_naves_asignadas():
+                if i.get_nombre() == nombre_nave:
+                    nave = i
+                    break
+            if nave is None:
+                raise NaveNoComandada(nombre_nave)
+            
+            nave.comprobar_repuesto(nombre_repuesto)
             print(f"El repuesto {nombre_repuesto} se encuentra entre los necesarios para la nave")
             return True
-
-    def adquirir_repuesto(self, nombre_repuesto, almacen, nombre_nave):
-        nave = None
-        for i in self.get_naves_asignadas():
-            if i.get_nombre() == nombre_nave:
-                nave = i
-                break
-        if nave is None:
-            raise NaveNoComandada(nombre_nave)
-
-        if nombre_repuesto not in nave.get_lista_repuestos():
-            print(f"El repuesto {nombre_repuesto} no está en la lista de repuestos de {nombre_nave}")
+        
+        except NaveNoComandada as e:
+            print(e)
+            return False
+        
+        except RepuestoDeOtraNave as e:
+            print(e)
             return False
 
-        for i in almacen.get_catalogo():
-            if i.get_nombre() == nombre_repuesto:
-                if i.get_stock() > 0:
-                    nuevo_stock = i.get_stock() - 1
-                    i.set_stock(nuevo_stock)
-                    print(f"Repuesto {nombre_repuesto} adquirido correctamente para la nave {nombre_nave}")
-                    return True 
-                else:
-                    raise RepuestoSinStock(i)
+    def adquirir_repuesto(self, nombre_repuesto, almacen, nombre_nave):
+        try:
+            nave = None
+            for i in self.get_naves_asignadas():
+                if i.get_nombre() == nombre_nave:
+                    nave = i
+                    break
+            if nave is None:
+                raise NaveNoComandada(nombre_nave)
+            
+            nave.comprobar_repuesto(nombre_repuesto)       # Si no sale salta el error y es cazado por el except
+            almacen.comprobar_repuesto(nombre_repuesto)
         
-        print(f"El repuesto {nombre_repuesto} no está en este almacén")
-        return False
+            for i in almacen.get_catalogo():
+                if i.get_nombre() == nombre_repuesto:
+                    if i.get_stock() > 0:
+                        nuevo_stock = i.get_stock() - 1
+                        i.set_stock(nuevo_stock)
+                        print(f"Repuesto {nombre_repuesto} adquirido correctamente para la nave {nombre_nave}")
+                        return True 
+                    else:
+                        raise RepuestoSinStock(i)
+        
+        except NaveNoComandada as e:
+            print(e)
+            return False
+        
+        except RepuestoDeOtraNave as e:
+            print(e)
+            return False
+        
+        except RepuestoNoEncontrado as e:
+            print(e)
+            return False
+        
+        except RepuestoSinStock as e:
+            print(e)
+            return False
--- src/OperarioAlmacen.py
+++ src/OperarioAlmacen.py
@@ -1,29 +1,36 @@
 from Usuario import Usuario
 from Repuesto import Repuesto
 from Almacen import Almacen
+from Errores import RepuestoNoEncontrado
 
 class OperarioAlmacen(Usuario):
     def __init__(self, id):
         super().__init__(id)
     
     def consultar_repuesto_almacen(self, nombre_repuesto, almacen):
-        if almacen.comprobar_repuesto(nombre_repuesto):
+        try:
+            almacen.comprobar_repuesto(nombre_repuesto)
             print(f"El repuesto {nombre_repuesto} se encuentra en el almacén")
-        else:
-            print(f"El repuesto {nombre_repuesto} no se encuentra en el almacén")
+            return True        
+        except RepuestoNoEncontrado as e:
+            print(e)
+            return False
 
     def actualizar_stock(self, repuesto, stock):
         nuevo_stock = repuesto.get_stock() + stock
         repuesto.set_stock(nuevo_stock)
 
     def actualizar_catalogo(self, repuesto, almacen):
-        if almacen.comprobar_repuesto(repuesto.get_nombre()):
+        try:
+            almacen.comprobar_repuesto(repuesto.get_nombre())
             for i in almacen.get_catalogo():
                 if i.get_nombre() == repuesto.get_nombre():
                     self.actualizar_stock(i, repuesto.get_stock())
-                    break 
-        else:
+                    print(f"Stock actualizado para {repuesto.get_nombre()}") 
+        except RepuestoNoEncontrado:
             almacen.get_catalogo().append(repuesto)
+            print(f"Repuesto {repuesto.get_nombre()} añadido al catálogo")
+
 
     def encuentra_repuesto(self, nombre_repuesto, imperio):
         for i in imperio.get_lista_almacenes():
